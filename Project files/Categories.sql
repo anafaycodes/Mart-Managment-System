168b720e4b6b37fc3d366e1546c410c7b870a5d5
Abdul Nafay <139160993+anafaycodes@users.noreply.github.com>
2024-05-30 21:01:26 +0500

    Create Categories.sql

---
+++ Project files/Categories.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Categories (
+    CategoryID INT PRIMARY KEY,
+    CategoryName VARCHAR(255)
+);
+
+-- Insert data into Categories table
+INSERT INTO Categories (CategoryID, CategoryName)
+VALUES
+(1, 'Electronics'),
+(2, 'Books'),
+(3, 'Clothing'),
+(4, 'Home Appliances');
